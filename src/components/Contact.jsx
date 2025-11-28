acb810d89c479ca7c607dc830578dc2a528c0017
adrianogdr <adrianogdr@hotmail.com>
2025-11-27 21:55:51 -0300

    chore: remover import não utilizado (CardContent) em Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,6 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Alert from '@mui/material/Alert';
